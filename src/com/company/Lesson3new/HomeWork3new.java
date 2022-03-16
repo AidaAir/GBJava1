ace7c2f1f66096809aecf935eb7bfd8d31ce1d5d
AidaAir <aida-da@mail.ru>
2022-03-16 20:16:40 +0300

    Правки по дз 3

--- src/com/company/Lesson3new/HomeWork3new.java
+++ src/com/company/Lesson3new/HomeWork3new.java
@@ -6,25 +6,23 @@ import java.util.Scanner;
 public class HomeWork3new {
 
     public static void main(String[] args) {
-             arrayOneZero();
-             oneToHundred();
-             multiplyForTwo();
-             square();
-             System.out.println(Arrays.toString(value(3, 6)));
+          arrayOneZero();
+          oneToHundred();
+          multiplyForTwo();
+          square();
+          System.out.println(Arrays.toString(value(3, 6)));
     }
 
     private static void arrayOneZero() {
         int[] first = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < first.length; i++) {
-            int value = first[i];
-            if (value == 0) {
-                System.out.print(1 + " ");
-            } else if (value == 1) {
-                System.out.print(0 + " ");
+            if (first[i] == 0) {
+                first[i] = 1;
+            } else {
+                first[i] = 0;
             }
         }
-        System.out.println();
-        System.out.println();
+        System.out.println(Arrays.toString(first));
     }
 
     private static void oneToHundred() {
@@ -32,33 +30,24 @@ public class HomeWork3new {
         for (int i = 0; i < hundred.length; i++) {
             System.out.println(i + 1);
         }
-        System.out.println();
     }
 
     private static void multiplyForTwo() {
         int[] multi = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < multi.length; i++) {
-            int num = multi[i];
-            if (num < 6) {
-                System.out.print(num * 2 + ", ");
+            if (multi[i] < 6) {
+                multi[i] = multi[i] * 2;
             }
         }
-        System.out.println();
-        System.out.println();
+        System.out.println(Arrays.toString(multi));
     }
 
     private static void square() {
-        int[][] s = new int[4][4];
-        for (int i = 0; i < s.length; i++) {
-            for (int w = 0; w < s.length; w++) {
-                if (i == w)
-                    s[i][w] = 1;
-            }
-            {
-                System.out.println(Arrays.toString(s[i]));
-            }
+        int[][] array = new int[4][4];
+        for (int i = 0; i < array.length; i++) {
+            array[i][i] = 1;
+            System.out.println(Arrays.toString(array[i]));
         }
-        System.out.println();
     }
 
     private static int[] value(int len, int initialValue) {
